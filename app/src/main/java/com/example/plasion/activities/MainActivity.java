46041917e7a8868177c2625fbd1f0e9260b5a0fd
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-01-03 23:21:01 +0700

    feat: added Login-Register UI

--- app/src/main/java/com/example/plasion/activities/MainActivity.java
+++ app/src/main/java/com/example/plasion/activities/MainActivity.java
@@ -1,16 +1,43 @@
 package com.example.plasion.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.plasion.R;
 
 public class MainActivity extends AppCompatActivity {
+    AppCompatButton buttonLogin;
+    AppCompatButton buttonRegister;
+
+    private final View.OnClickListener loginClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    private final View.OnClickListener registerClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonLogin = (AppCompatButton) findViewById(R.id.login_button);
+        buttonRegister = (AppCompatButton) findViewById(R.id.register_button);
+
+        buttonLogin.setOnClickListener(loginClickListener);
+        buttonRegister.setOnClickListener(registerClickListener);
     }
 }
