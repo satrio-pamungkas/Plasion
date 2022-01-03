89e173ff5b2c53bc717d98ac968b238fbda1a1db
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-01-03 21:56:18 +0700

    feat: added MainScreen

--- app/src/main/java/com/example/plasion/activities/SplashScreen.java
+++ app/src/main/java/com/example/plasion/activities/SplashScreen.java
@@ -1,17 +1,19 @@
-package com.example.plasion;
+package com.example.plasion.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Window;
-import android.view.WindowManager;
+
+import com.example.plasion.R;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
